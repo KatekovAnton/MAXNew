89d0128325e57ea89e390f5e346b4b7323470ea6
Anton Katekov <void_main@iMac-Anton.local>
2013-02-01 14:42:13 +0400

    progress in animations

---
+++ MAX/Game/Game/Objects/Effects/GameEffectBlast.h
@@ -0,0 +1,14 @@
+//
+//  GameEffectBlast.h
+//  MAX
+//
+//  Created by Anton Katekov on 28.12.12.
+//  Copyright (c) 2012 AntonKatekov. All rights reserved.
+//
+
+#ifndef __MAX__GameEffectBlast__
+#define __MAX__GameEffectBlast__
+
+#include <iostream>
+
+#endif /* defined(__MAX__GameEffectBlast__) */
---
+++ MAX/Game/Game/Objects/Effects/GameEffectRocket.cpp
@@ -0,0 +1,9 @@
+//
+//  GameEffectRocket.cpp
+//  MAX
+//
+//  Created by Anton Katekov on 28.12.12.
+//  Copyright (c) 2012 AntonKatekov. All rights reserved.
+//
+
+#include "GameEffectRocket.h"
