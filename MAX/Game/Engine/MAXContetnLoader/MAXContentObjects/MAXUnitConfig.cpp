e17714ed3938fc834e35b76ab53f41e9a46436a8
Anton Katekov <void_main@iMac-Anton.local>
2013-02-16 00:04:07 +0400

    config

--- MAX/Game/Engine/MAXContetnLoader/MAXContentObjects/MAXUnitConfig.cpp
+++ MAX/Game/Engine/MAXContetnLoader/MAXContentObjects/MAXUnitConfig.cpp
@@ -65,6 +65,19 @@ std::vector<std::string> splitString(const std::string &s, string delim)
     return elems;
 }
 
+std::vector<std::string> getElements(string value)
+{
+    size_t index = 0;
+    while (true) {
+        index = value.find("  ", 0);
+        if (index == string::npos)
+            break;
+        value.replace(index, 2, " ");
+    }
+    vector<string> elements = splitString(value, ' ');
+    return elements;
+}
+    
 const string commentSign = "//";
 
 vector<string> removeCommentsAndEmptyStrings(vector<string> lines)
@@ -162,6 +175,196 @@ void MAXUnitConfig::SetResurceConfigValue(string key, string value)
     
 }
 
+void MAXUnitConfig::SetBalanceConfigValue(string key, string value)
+{
+    unsigned char name[3] = {0xC8, 0xEC, 0xFF};
+    if (memcmp(name, key.c_str(), 3) == 0 && false) {
+        _name = value;
+        return;
+    }
+    
+    unsigned char nameEng[7] = {0xC8, 0xEC, 0xFF, 0x2D, 0x45, 0x6E, 0x67};
+    if (memcmp(nameEng, key.c_str(), 3) == 0 && false) {
+        _name = value;
+        return;
+    }
+    
+    if (key == "Basics")
+    {
+        //Basics=1   0   4   52  4   0
+        ////     Р   П   У   Н   С   С
+        ////     а   р   р   о   т   т
+        ////     з   о   о   м   р   р
+        ////     м   х   в   е   о   о
+        ////     е   о   е   р   и   и
+        ////     р   д   н       м   т
+        ////         и   ь       ы   е
+        ////         м           й   л
+        ////         о               ь
+        ////         с
+        ////         т
+        ////         ь
+        
+        vector<string> elements = getElements(value);
+        _bSize               = atoi(elements[0].c_str());
+        _bMoveType           = atoi(elements[1].c_str());
+        _bLevel              = atoi(elements[2].c_str());
+        _bNumber             = atoi(elements[3].c_str());
+        _bSelfCreatedType    = atoi(elements[4].c_str());
+        _bSelfCreatorType    = atoi(elements[5].c_str());
+        return;
+    }
+    
+    if (key == "Features")
+    {
+        //Features=00000000000000000000000001010000
+        ////       ЗПВНАСВШЧКНМДПБРДММСОПИПГЗОПАПВВ
+        ////       досебтиаеоаооруеиииачесоевбоноии
+        ////       адевседхлнссролмвннмкрссоенкиддд
+        ////       нкгынлитонттовьоеаооиелалзоумвии
+        ////       илдбеставер годнр урдзедодвпаотт
+        ////       еавив с еко адотс кеаадоголацдпм
+        ////        дкры т кте  нзеа лмёруч пееинои
+        ////        клаб е  ом  иерн аотяен онмяыдн
+        ////        а еи л  ры  кр т дн жты риы йвы
+        ////          мр с   й       чт а й тяй  о
+        ////           а             и  е        д
+        ////           е             к  т        о
+        ////           м                         й
+        _isBuilding         = value[0] == 0x31;
+        _isNeedPodkladka    = value[1] == 0x31;
+        _isAllwaysOn        = value[2] == 0x31;
+        _isCantSelect       = value[3] == 0x31;
+        _isAbsCantSelect    = value[4] == 0x31;
+        _isStealth          = value[5] == 0x31;
+        _isAntiStealth      = value[6] == 0x31;
+        _isMine             = value[7] == 0x31;
+        _isInfantry         = value[8] == 0x31;
+        _isConnector        = value[9] == 0x31;
+        _isCantCreate       = value[10] == 0x31;
+        _isBridge           = value[11] == 0x31;
+        _isRoad             = value[12] == 0x31;
+        _isRetranslator     = value[13] == 0x31;
+        _isBuldozer         = value[14] == 0x31;
+        _isRepair           = value[15] == 0x31;
+        _isInfiltrator      = value[16] == 0x31;
+        _isBombMine         = value[17] == 0x31;
+        _isBombMinelayer    = value[18] == 0x31;
+        _isAutorepair       = value[19] == 0x31;
+        _isGivePoints       = value[20] == 0x31;
+        _isReloader         = value[21] == 0x31;
+        _isResearch         = value[22] == 0x31;
+        _isLanding          = value[23] == 0x31;
+        _isSurvivor         = value[24] == 0x31;
+        _isSpacePort        = value[25] == 0x31;
+        _isUpgrades         = value[26] == 0x31;
+        _isCanBuy           = value[27] == 0x31;
+        _isAnimated         = value[28] == 0x31;
+        _isUnderwater       = value[29] == 0x31;
+        _isSeeUnderwater    = value[30] == 0x31;
+        _isSeeMines         = value[31] == 0x31;
+        return;
+    }
+    
+    if (key == "Parameters")
+    {
+        //Parameters=6   24  10  16  2   42  4   4   14  12  0   0   1   1   1   0
+        ////         С   П   Б   А   В   Т   Д   В   П   Ц   З   Х   Т   Т   О   М
+        ////         к   р   р   т   ы   о   и   и   а   е   о   о   и   и   т   а
+        ////         о   о   о   а   с   п   а   д   т   н   н   д   п   п   д   т
+        ////         р   ч   н   к   т   л   п   м   р   а   а   о   о   с   .
+        ////         о   н   я   а   р   и   а   о   о           с   г   т   п   в
+        ////         с   о           е   в   з   с   н           т   н   р   у
+        ////         т   с           л   о   о   т   ы           р   я   е   ш   х
+        ////         ь   т           ы       н   ь               е       л   к   о
+        ////             ь                                       л           а   д
+        
+        vector<string> elements = getElements(value);
+        
+        _pSpeed             = atoi(elements[0].c_str());
+        _pHealth            = atoi(elements[1].c_str());
+        _pArmor             = atoi(elements[2].c_str());
+        _pAttack            = atoi(elements[3].c_str());
+        _pShots             = atoi(elements[4].c_str());
+        _pFuel              = atoi(elements[5].c_str());
+        _pRange             = atoi(elements[6].c_str());
+        _pScan              = atoi(elements[7].c_str());
+        _pAmmo              = atoi(elements[8].c_str());
+        _pCost              = atoi(elements[9].c_str());
+        _pZone              = atoi(elements[10].c_str());
+        _pMoveAndShot       = atoi(elements[11].c_str());
+        _pFireType          = atoi(elements[12].c_str());
+        _pBulletType        = atoi(elements[13].c_str());
+        _pSeparateCanon     = atoi(elements[14].c_str());
+        _pMatPerTurn        = atoi(elements[15].c_str());
+        return;
+    }
+    
+    if (key == "Resources")
+    {
+        //Resources=0   0   0   0   0
+        vector<string> elements = getElements(value);
+        _rMaterial          = atoi(elements[0].c_str());
+        _rFuel              = atoi(elements[1].c_str());
+        _rGold              = atoi(elements[2].c_str());
+        _rEnergy            = atoi(elements[3].c_str());
+        _rPeoples           = atoi(elements[4].c_str());
+        
+        return;
+    }
+ 
+    if (key == "Store")
+    {
+        //Store=0  0  0  0
+        vector<string> elements = getElements(value);
+        _sGround            = atoi(elements[0].c_str());
+        _sAir               = atoi(elements[1].c_str());
+        _sSea               = atoi(elements[2].c_str());
+        _sInfantry          = atoi(elements[3].c_str());
+        
+        return;
+    }
+    
+    if (key == "Needs")
+    {
+        //Needs  =0   0   0   0   0   0
+        vector<string> elements = getElements(value);
+        _nMaterial          = atoi(elements[0].c_str());
+        _nFuel              = atoi(elements[1].c_str());
+        _nGold              = atoi(elements[2].c_str());
+        _nEnergy            = atoi(elements[3].c_str());
+        _nPeoples           = atoi(elements[4].c_str());
+        
+        return;
+    }
+    
+    if (key == "Returns")
+    {
+        //Returns=0   0   0   0   0   0
+        vector<string> elements = getElements(value);
+        _retMaterial            = atoi(elements[0].c_str());
+        _retFuel                = atoi(elements[1].c_str());
+        _retGold                = atoi(elements[2].c_str());
+        _retEnergy              = atoi(elements[3].c_str());
+        _retPeoples             = atoi(elements[4].c_str());
+        
+        return;
+    }
+    
+    
+    //##begin_description(eng)
+    //Heavily armored fighting vehicle.
+    //Best used in the front line to prevent enemy units from reaching lightly-armored support units such as assault guns and rocket launchers.
+    //##end_description
+    //
+    //##begin_description(rus)
+    //Танк, это машина поля боя - тяжелобронированная медленная и мощная боевая техника, стреляющая обычными снарядами.
+    //Обладая мощной броней танки всегда на острие атаки, вызывая огонь на себя, они взламывают любую оборону.
+    //Эффективны против любой наземной техники.
+    //##end_description
+
+}
+
 MAXUnitConfig::MAXUnitConfig(string balanceConfigName, string resourceConfigName)
 :_bodyName(""), _imageName(""), _shadowName(""), _instoreName(""), _type(""), _isAnimBase(false), _isAnimHead(false), _isAbleToFire(false)
 {
@@ -180,72 +383,18 @@ MAXUnitConfig::MAXUnitConfig(string balanceConfigName, string resourceConfigName
     
     r = new BinaryReader(balanceConfigName);
     string balanceConfig = r->ReadFullAsString();
+    std::remove(balanceConfig.begin(), balanceConfig.end(), '\r');
     delete r;
-    //Тип=Tank
-    //Имя=Танк
-    //Имя-Eng=Tank
-    //
-    //Basics=1   0   4   52  4   0
-    ////     Р   П   У   Н   С   С
-    ////     а   р   р   о   т   т
-    ////     з   о   о   м   р   р
-    ////     м   х   в   е   о   о
-    ////     е   о   е   р   и   и
-    ////     р   д   н       м   т
-    ////         и   ь       ы   е
-    ////         м           й   л
-    ////         о               ь
-    ////         с
-    ////         т
-    ////         ь
-    //
-    //Features=00000000000000000000000001010000
-    ////       ЗПВНАСВШЧКНМДПБРДММСОПИПГЗОПАПВВ
-    ////       досебтиаеоаооруеиииачесоевбоноии
-    ////       адевседхлнссролмвннмкрссоенкиддд
-    ////       нкгынлитонттовьоеаооиелалзоумвии
-    ////       илдбеставер годнр урдзедодвпаотт
-    ////       еавив с еко адотс кеаадоголацдпм
-    ////        дкры т кте  нзеа лмёруч пееинои
-    ////        клаб е  ом  иерн аотяен онмяыдн
-    ////        а еи л  ры  кр т дн жты риы йвы
-    ////          мр с   й       чт а й тяй  о
-    ////           а             и  е        д
-    ////           е             к  т        о
-    ////           м                         й
-    //
-    //Parameters=6   24  10  16  2   42  4   4   14  12  0   0   1   1   1   0
-    ////         С   П   Б   А   В   Т   Д   В   П   Ц   З   Х   Т   Т   О   М
-    ////         к   р   р   т   ы   о   и   и   а   е   о   о   и   и   т   а
-    ////         о   о   о   а   с   п   а   д   т   н   н   д   п   п   д   т
-    ////         р   ч   н   к   т   л   п   м   р   а   а   о   о   с   .
-    ////         о   н   я   а   р   и   а   о   о           с   г   т   п   в
-    ////         с   о           е   в   з   с   н           т   н   р   у
-    ////         т   с           л   о   о   т   ы           р   я   е   ш   х
-    ////         ь   т           ы       н   ь               е       л   к   о
-    ////             ь                                       л           а   д
-    //
-    //Resources=0   0   0   0   0
-    ////        мат топ зол эн  люд
-    //
-    //Store=0  0  0  0
-    ////    Ю  В  М  П
-    //
-    //Needs  =0   0   0   0   0   0
-    //Returns=0   0   0   0   0   0
-    ////      мат топ зол гзл энр люд
-    //
-    //##begin_description(eng)
-    //Heavily armored fighting vehicle.
-    //Best used in the front line to prevent enemy units from reaching lightly-armored support units such as assault guns and rocket launchers.
-    //##end_description
-    //
-    //##begin_description(rus)
-    //Танк, это машина поля боя - тяжелобронированная медленная и мощная боевая техника, стреляющая обычными снарядами.
-    //Обладая мощной броней танки всегда на острие атаки, вызывая огонь на себя, они взламывают любую оборону.
-    //Эффективны против любой наземной техники.
-    //##end_description
-}
+    lines = splitString(balanceConfig, '\n');
+    lines = removeCommentsAndEmptyStrings(lines);
+    for (int i = 0; i < lines.size(); i++)
+    {
+        vector<string> typeData = splitString(lines[i], '=');
+        if (typeData.size() == 2)
+            SetBalanceConfigValue(typeData[0], typeData[1]);
+    }
+    
+    }
 
 MAXUnitConfigManager* MAXUnitConfigManager::SharedMAXUnitConfigManager()
 {
--- MAX/Game/Engine/MAXContetnLoader/MAXContentObjects/MAXUnitConfig.h
+++ MAX/Game/Engine/MAXContetnLoader/MAXContentObjects/MAXUnitConfig.h
@@ -12,10 +12,22 @@
 #include <iostream>
 #include "miniPrefix.h"
 
+typedef enum
+{
+    UNIT_LEVEL_UNDERWATER   = 0,
+    UNIT_LEVEL_ONGROUND     = 1,
+    UNIT_LEVEL_OVERGROUND   = 2,
+    UNIT_LEVEL_BUILDING     = 3,
+    UNIT_LEVEL_UNITS        = 4,
+    UNIT_LEVEL_OVERUNITS    = 5,
+    UNIT_LEVEL_INAIR        = 6,
+    UNIT_LEVEL_OVERAIR      = 7
+} UNIT_LEVEL;
+
 class MAXUnitConfig {
     
     void SetResurceConfigValue(string key, string value);
-    
+    void SetBalanceConfigValue(string key, string value);
 public:
     //Тип=Tank
     string _type;
@@ -51,6 +63,121 @@ public:
     //Стопзвук=off.wav
     //Двигзвук=run.wav
     //Степзвук=off.wav
+    
+    
+    string _name;
+    //Basics
+    int _bSize;
+    int _bMoveType;
+    int _bLevel;
+    int _bNumber;
+    int _bSelfCreatedType;
+    int _bSelfCreatorType;
+    
+    //Features=00000000000000000000000001010000
+    ////       ЗПВНАСВШЧКНМДПБРДММСОПИПГЗОПАПВВ
+    ////       досебтиаеоаооруеиииачесоевбоноии
+    ////       адевседхлнссролмвннмкрссоенкиддд
+    ////       нкгынлитонттовьоеаооиелалзоумвии
+    ////       илдбеставер годнр урдзедодвпаотт
+    ////       еавив с еко адотс кеаадоголацдпм
+    ////        дкры т кте  нзеа лмёруч пееинои
+    ////        клаб е  ом  иерн аотяен онмяыдн
+    ////        а еи л  ры  кр т дн жты риы йвы
+    ////          мр с   й       чт а й тяй  о
+    ////           а             и  е        д
+    ////           е             к  т        о
+    ////           м                         й
+    bool _isBuilding;
+    bool _isNeedPodkladka;
+    bool _isAllwaysOn;
+    bool _isCantSelect;
+    bool _isAbsCantSelect;
+    bool _isStealth;
+    bool _isAntiStealth;
+    bool _isMine;
+    bool _isInfantry;
+    bool _isConnector;
+    bool _isCantCreate;
+    bool _isBridge;
+    bool _isRoad;
+    bool _isRetranslator;
+    bool _isBuldozer;
+    bool _isRepair;
+    bool _isInfiltrator;
+    bool _isBombMine;
+    bool _isBombMinelayer;
+    bool _isAutorepair;
+    bool _isGivePoints;
+    bool _isReloader;
+    bool _isResearch;
+    bool _isLanding;
+    bool _isSurvivor;
+    bool _isSpacePort;
+    bool _isUpgrades;
+    bool _isCanBuy;
+    bool _isAnimated;
+    bool _isUnderwater;
+    bool _isSeeUnderwater;
+    bool _isSeeMines;
+    
+    //Parameters=6   24  10  16  2   42  4   4   14  12  0   0   1   1   1   0
+    ////         С   П   Б   А   В   Т   Д   В   П   Ц   З   Х   Т   Т   О   М
+    ////         к   р   р   т   ы   о   и   и   а   е   о   о   и   и   т   а
+    ////         о   о   о   а   с   п   а   д   т   н   н   д   п   п   д   т
+    ////         р   ч   н   к   т   л   п   м   р   а   а   о   о   с   .
+    ////         о   н   я   а   р   и   а   о   о           с   г   т   п   в
+    ////         с   о           е   в   з   с   н           т   н   р   у
+    ////         т   с           л   о   о   т   ы           р   я   е   ш   х
+    ////         ь   т           ы       н   ь               е       л   к   о
+    ////             ь                                       л           а   д
+    int _pSpeed;
+    int _pHealth;
+    int _pArmor;
+    int _pAttack;
+    int _pShots;
+    int _pFuel;
+    int _pRange;
+    int _pScan;
+    int _pAmmo;
+    int _pCost;
+    int _pZone;
+    int _pMoveAndShot;
+    int _pFireType;
+    int _pBulletType;
+    int _pSeparateCanon;
+    int _pMatPerTurn;
+    
+    //Resources=0   0   0   0   0
+    ////        мат топ зол эн  люд
+    int _rMaterial;
+    int _rFuel;
+    int _rGold;
+    int _rEnergy;
+    int _rPeoples;
+    
+    
+    //Store=0  0  0  0
+    ////    Ю  В  М  П
+    int _sGround;
+    int _sAir;
+    int _sSea;
+    int _sInfantry;
+    
+    //Needs  =0   0   0   0   0   0
+    //Returns=0   0   0   0   0   0
+    ////      мат топ зол гзл энр люд
+    int _nMaterial;
+    int _nFuel;
+    int _nGold;
+    int _nEnergy;
+    int _nPeoples;
+    
+    int _retMaterial;
+    int _retFuel;
+    int _retGold;
+    int _retEnergy;
+    int _retPeoples;
     
     MAXUnitConfig(string balanceConfigName, string resourceConfigName);
     bool IsValid() const;
