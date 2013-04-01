4194a4c0c9448ebd889cd29c4b59d3de0cbb4c8a
Hruks <hruks@mail.ru>
2013-04-01 15:05:35 +0400

    small rework on MAXAnimationObject

--- MAX/Game/Engine/Animation/MAXAnimationObject.h
+++ MAX/Game/Engine/Animation/MAXAnimationObject.h
@@ -28,18 +28,19 @@ class MAXAnimationObject : public MAXAnimationBase {
     MAXObject* _unit;
     CCPoint _startLocation;
     CCPoint _endLocation;
+    CCPoint _deltaLocation;
     
 public:
     
     int _moveCurve;
-    float _moveTime;
+    float _aniTime;
     
     CCPoint GetStartLocation () const { return _startLocation; };
     CCPoint GetEndLocation () const { return _endLocation; };
     
     MAXAnimationObject(const CCPoint& startLocation, const CCPoint& endLocation, MAXObject* object);      //creates move action
     
-    
+    float GetAniElapsedPart(double elapsedTime);
     virtual bool IsFinished();
     virtual void Update(double time);
     virtual void CompletlyFinish();
