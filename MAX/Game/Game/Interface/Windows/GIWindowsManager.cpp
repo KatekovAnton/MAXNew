261b5cb774e37f9f95cc50919033ba01683eda02
 Developer <developer@Developers-iMac.local>
2013-05-27 01:58:23 +0400

    windows manager class

---
+++ MAX/Game/Game/Interface/Windows/GIWindowsManager.cpp
@@ -0,0 +1,15 @@
+//
+//  GIWindowsManager.cpp
+//  MAX
+//
+//  Created by  Developer on 27.05.13.
+//  Copyright (c) 2013 AntonKatekov. All rights reserved.
+//
+
+#include "GIWindowsManager.h"
+
+class GIWindowsManager {
+    
+public:
+    
+};
---
+++ MAX/Game/Game/Interface/Windows/GIWindowsManager.h
@@ -0,0 +1,14 @@
+//
+//  GIWindowsManager.h
+//  MAX
+//
+//  Created by  Developer on 27.05.13.
+//  Copyright (c) 2013 AntonKatekov. All rights reserved.
+//
+
+#ifndef __MAX__GIWindowsManager__
+#define __MAX__GIWindowsManager__
+
+#include <iostream>
+
+#endif /* defined(__MAX__GIWindowsManager__) */
