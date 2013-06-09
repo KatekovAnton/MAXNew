a49613cad48d23bd56508f0afa028f50befbff17
Katekov Anton <void0main@gmail.com>
2013-06-10 02:35:12 +0400

    fixed render system

--- MAX/Game/Engine/Renderer/RenderSystem.cpp
+++ MAX/Game/Engine/Renderer/RenderSystem.cpp
@@ -13,20 +13,9 @@
 RenderSystem::RenderSystem() {
 }
 
-void RenderSystem::Init() {
-#ifdef TARGET_OS_IPHONE
-    _deviceDisplay = Display::currentDisplay();
-    _deviceDisplay->InitGLSurface(GL_DEPTH_COMPONENT24_OES, GL_RGB565);
-#endif
-}
-
 void RenderSystem::InitOpenGL() {
     _deviceDisplay = Display::currentDisplay();
     glViewport(0, 0, _deviceDisplay->GetDisplayWidth(), _deviceDisplay->GetDisplayHeight());
-}
-
-void RenderSystem::SetColor(float color){
-    _color = color;
 }
 
 void RenderSystem::EndFrame() {
